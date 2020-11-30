4f4a5b1e937f94cd33c97b6e3ea70e8926f1537c
ras <rasim.serif.balkan@gmail.com>
2020-11-30 20:09:08 +0300

    clean code

--- FreeThumbnail.Library/Creator.cs
+++ FreeThumbnail.Library/Creator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.IO;
 
 namespace FreeThumbnail.Library
@@ -10,38 +9,34 @@ namespace FreeThumbnail.Library
     {
         public Image CreateThumbnail(int height, int weight, Stream file)
         {
-            Bitmap bitmapFile = new Bitmap(file);
-            ImageFormat rawFormat = bitmapFile.RawFormat;
+            var bitmapFile = new Bitmap(file);
             int sourceHeight;
             int truncate;
             int heightRound;
             int num4;
             if (bitmapFile.Width > bitmapFile.Height)
             {
-                Decimal d2 = Decimal.Divide(new Decimal(height), new Decimal(bitmapFile.Width));
+                var d2 = decimal.Divide(new decimal(height), new decimal(bitmapFile.Width));
                 sourceHeight = height;
-                truncate = Convert.ToInt32(Math.Truncate(Decimal.Multiply(new Decimal(bitmapFile.Height), d2)));
+                truncate = Convert.ToInt32(Math.Truncate(decimal.Multiply(new decimal(bitmapFile.Height), d2)));
                 heightRound = 0;
-                num4 = checked((int)Math.Round(unchecked((double)checked(weight - truncate) / 2.0)));
+                num4 = checked((int)Math.Round(checked(weight - truncate) / 2.0));
             }
             else
             {
-                Decimal d2 = Decimal.Divide(new Decimal(weight), new Decimal(bitmapFile.Height));
+                var d2 = decimal.Divide(new decimal(weight), new decimal(bitmapFile.Height));
                 truncate = weight;
-                sourceHeight = Convert.ToInt32(Math.Truncate(Decimal.Multiply(new Decimal(bitmapFile.Width), d2)));
+                sourceHeight = Convert.ToInt32(Math.Truncate(decimal.Multiply(new decimal(bitmapFile.Width), d2)));
                 num4 = 0;
-                heightRound = checked((int)Math.Round(unchecked((double)checked(height - sourceHeight) / 2.0)));
+                heightRound = checked((int)Math.Round(checked(height - sourceHeight) / 2.0));
             }
-            Bitmap result = new Bitmap(height, weight);
-            Graphics graphics = Graphics.FromImage((Image)result);
+            var result = new Bitmap(height, weight);
+            var graphics = Graphics.FromImage(result);
             graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
             graphics.FillRectangle(Brushes.White, 0, 0, height, weight);
-            graphics.DrawImage((Image)bitmapFile, (float)heightRound, (float)num4, (float)sourceHeight, (float)truncate);
+            graphics.DrawImage(bitmapFile, heightRound, num4, (float)sourceHeight, truncate);
 
             return result;
-
-
-
         }
     }
 }
